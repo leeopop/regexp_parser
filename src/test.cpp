a2fc8ece09875e7d7114d2c785057770905bea9f
leeopop <dlrmsghd@gmail.com>
2015-03-30 23:53:11 +0900

    lexer seems working

--- src/test.cpp
+++ src/test.cpp
@@ -5,10 +5,41 @@
  *      Author: 근홍
  */
 
-
+extern "C"
+{
+#include "regexp_lex.h"
+#include "regexp_yacc.h"
+}
 
 
 int main()
 {
+	while(true)
+	{
+		int ret = yylex();
+		if(ret == 0)
+		{
+			fflush(0);
+			yyrestart(stdin);
+			continue;
+		}
+		if(ret == -1)
+		{
+			printf("Error:\n");
+			break;
+		}
+		if(ret == yytokentype::CHAR)
+		{
+			printf("CHAR:%s\n", yytext);
+		}
+		else if(ret == yytokentype::ESCAPED_CHAR)
+		{
+			printf("ESCAPED_CHAR:%s\n", yytext);
+		}
+		else
+		{
+			printf("ELSE:%s\n", yytext);
+		}
+	}
 	return 0;
 }
